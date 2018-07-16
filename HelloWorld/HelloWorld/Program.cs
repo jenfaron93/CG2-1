2d632c1c733d82ced0cc31590dbeeea8837b81c2
Jennifer Faron <jf891w@gmail.com>
2018-07-15 19:57:48 -0500

    Assigns input to strings and then reads out

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Please enter your name.");
+			Console.Write("Type your first name: ");
+			string myFirstName = Console.ReadLine();
+
+			Console.Write("Type your last name: ");
+			string myLastName = Console.ReadLine();
+
+			Console.Write("Hello" + " " + myFirstName + " " + myLastName + "!");
+			Console.ReadLine();
+        }
+    }
+}
